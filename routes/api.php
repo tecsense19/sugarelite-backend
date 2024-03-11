00acd21f9ffc3c076c245e0bd9499ea18584b46c
tecsense19 <tecsense19@gmail.com>
2024-03-11 17:23:03 +0530

    online status API, and push Notification

--- routes/api.php
+++ routes/api.php
@@ -25,26 +25,34 @@ Route::group(['prefix' => 'V1'], function () {
     Route::post('/profile/register', [SugareliteController::class, 'register']);
     Route::post('/checkUser', [SugareliteController::class, 'checkUser']);
     Route::post('/login', [SugareliteController::class, 'login']);
+    Route::post('/logout', [SugareliteController::class, 'logout']);
     Route::post('/chat/send', [SugareliteController::class, 'sendMessage']);
     Route::get('/chat/list', [SugareliteController::class, 'messageList']);
 
-    ///profileList
+    ///ProfileList
     Route::get('/profile/list', [SugareliteController::class, 'profileList']);
 
-    //friendLlist
+    //FriendLlist
     Route::post('/friends', [SugareliteController::class, 'friend_list']);
     Route::post('/profile/friends', [SugareliteController::class, 'friend_profile_list']);
-    
+
+    //Access
     Route::post('/privateimages/access', [SugareliteController::class, 'private_album']);
+
+    //Push
+    Route::get('/push/privatealbum', [SugareliteController::class, 'push_notifications_private_album']);
+    Route::get('/push/friendrequest', [SugareliteController::class, 'push_notifications_friend_request']);
     
-    
-    
+    //Newsletter
     Route::post('/newsletter', [NewslettersController::class, 'newsletter']);
 
+    //Password
     Route::post('/forgot/password', [SugareliteController::class, 'forgotPassword']);
 
     Route::post('/create/subscription', [StripeController::class, 'createSubscription']);
     Route::post('/start/stop/subscription', [StripeController::class, 'startStopSubscription']);
     Route::post('/cancel/subscription', [StripeController::class, 'cancelSubscription']);
+
+   
 });
 
